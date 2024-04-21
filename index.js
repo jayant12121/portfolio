a330e96208e501dfcec8340f17f88c1ad5a09474
jayant12121 <165195385+jayant12121@users.noreply.github.com>
2024-04-21 19:41:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__portpolio, 'index.html'));
+app.get('/index', (req, res) => {
+    res.sendFile(path.join( 'index.html'));
 });
 
 app.listen(port, () => {
